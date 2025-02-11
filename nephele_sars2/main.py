cef7ab268fab8554d9275a662ac5bf13471b4a3f
basnetb3 <basnetb3@nih.gov>
2025-02-11 08:21:03 -0800

    take pipeline args and mapping file path from terminal args (#2)
    * parser and enums
    
    * fix lint error
    
    * fix lint error
    
    * update ArgumentParser.get_samples method
    
    * update main
    
    * workflows: update build-and-push; build-and-push-base-image and set-image-pointer
    
    * Dockerfile.{base,update}
    
    * update Dockerfile
    
    * update utils version
    
    * update Datafiles section of readme.md
    
    * create .dict and .fai file for ref_db
    
    * update parser for ref_db_path; pull ref_db_path directory as the pipeline requires the .dict and .fai file corresponding to ref_db_path fa file
    
    * download snpEff_v4_3_core with dockerfile
    
    * update Dockerfile.base
    
    * update snpEff download loc with dockerfile
    
    * update snpeff version to 5.2c
    
    * create .dict and .fai file for ref_db
    
    * set type of ref_db_path to db file path
    
    * take ref_db_path as directory and set ref_db_path to point to SARS-CoV2.fa within that directory; the other supporting files for .fa within that directory are used by the pipeline too
    
    * update readme.Datafiles
    
    * update workflow/main*.nf to skip description while creating pairs
    
    * revert change to workflow/main*.nf; parser.ArgumentParser remove Description from mapping_file
    
    * update ArgumentParser.parse_args

--- nephele_sars2/main.py
+++ nephele_sars2/main.py
@@ -1,24 +1,15 @@
-import argparse
-import csv
 import os
 import shlex
 import shutil
 import subprocess
-import sys
-import traceback
-from pathlib import Path
+
+from nephele_pipeline_utils.decorators import pre_post_step
+from nephele_pipeline_utils.exceptions import NephelePipelineError
+from nephele_pipeline_utils.utils import log
 
 from config import pipeline_config
-from nephele_pipeline_utils.exceptions import NephelePipelineError
-from nephele_pipeline_utils.utils import log, read_json
-from pydantic import ValidationError
-from schema import (
-    DataType,
-    Sars2ARTICPipelinePE,
-    Sars2ARTICPipelineSE,
-    Sars2SGSPipelinePE,
-    Sars2SGSPipelineSE,
-)
+from enums import DataType
+from parser import parse_args
 
 
 def remove_intermediate_dirs(dname, dirs_to_keep):
@@ -36,130 +27,80 @@ def remove_intermediate_dirs(dname, dirs_to_keep):
             shutil.rmtree(dir_path, ignore_errors=True)
 
 
-def main(args):
+def post_clean_up(args):
     try:
-        exit_status = 0
-        report_outputs = False
-        data = read_json(args.json_file_path)
-        data_type = data.get("pipeline_arguments", {}).get("data_type", None)
-        if data_type == DataType.SGS_PE:
-            pipeline = Sars2SGSPipelinePE(**data)
-        elif data_type == DataType.SGS_SE:
-            pipeline = Sars2SGSPipelineSE(**data)
-        elif data_type == DataType.ARTIC_PE:
-            pipeline = Sars2ARTICPipelinePE(**data)
-        elif data_type == DataType.ARTIC_SE:
-            pipeline = Sars2ARTICPipelineSE(**data)
+        log("Removing intermediate dirs...")
+        dirs_to_keep = []
+        if args.get_bam_files:
+            dirs_to_keep.append("bam_files")
+        if args.data_type in [DataType.SGS_PE, DataType.SGS_SE]:
+            dirs_to_keep.extend(pipeline_config.sgs_dirs_to_keep)
         else:
-            raise ValidationError(f"Invalid data type: {data_type}")
-        args = pipeline.pipeline_arguments
-        outputs_dir_path = args.outputs_dir_path
-        report_outputs = True
-        # check Dependencies version
-        try:
-            log("Dependencies:")
-            log("--------------------------------")
-            dep = subprocess.run(
-                shlex.split("./dependencies.sh"),
-                stderr=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-            )
-            dep_stdout = dep.stdout.decode()
-            log(dep_stdout)
-            log("--------------------------------")
-        except Exception as _:
-            log("Warning: failed to check dependencies version.")
-        # Select script
-        if args.data_type == DataType.SGS_PE:
-            script_name = pipeline_config.pe_script_path
-        elif args.data_type == DataType.SGS_SE:
-            script_name = pipeline_config.se_script_path
-        elif args.data_type == DataType.ARTIC_PE:
-            script_name = f"{pipeline_config.artic_script_path} --data_type PE"
-        else:
-            script_name = f"{pipeline_config.artic_script_path} --data_type SE"
-        log(pipeline)
-        # Generate mapping file
-        mapping_file_path = outputs_dir_path / pipeline_config.mapping_file_name
-        pipeline.generate_mapping_file(mapping_file_path, file_name_only=False)
-        # Get ref_db_path params
-        ref_db_path = args.ref_db_path
-        # Get snpeff_db_path
-        snpeff_db_path = args.snpeff_db_path
-        # Command to execute the Nextflow pipeline
-        command = (
-            f"nextflow run -name {pipeline_config.nextflow_project_name} -c {pipeline_config.nextflow_config_path} -work-dir {pipeline_config.nextflow_work_dir} {script_name} "
-            f"--outputs_dir {outputs_dir_path} --map_file {mapping_file_path} --ref_db_path {ref_db_path} --snpeff_db_path {snpeff_db_path}"
+            dirs_to_keep.extend(pipeline_config.artic_dirs_to_keep)
+        remove_intermediate_dirs(args.outputs_dir_path, dirs_to_keep)
+    except Exception as e:
+        log(f"Warning: clean up step error: {str(e)}")
+
+
+@pre_post_step(parse_args, pipeline_config, post_clean_up)
+def main(args):
+    outputs_dir_path = args.outputs_dir_path
+
+    # check Dependencies version
+    try:
+        log("Dependencies:")
+        log("--------------------------------")
+        dep = subprocess.run(
+            shlex.split("./dependencies.sh"),
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE,
         )
-        # For one-pool/Artic, we need to pass the primer filepath
-        if args.data_type in [DataType.ARTIC_PE, DataType.ARTIC_SE]:
-            command += f" --primer_file_path {args.primer_file_path}"
-        log(command)
-        # Run
-        command_args = shlex.split(command)
+        dep_stdout = dep.stdout.decode()
+        log(dep_stdout)
+        log("--------------------------------")
+    except Exception as _:
+        log("Warning: failed to check dependencies version.")
+    # Select script
+    if args.data_type == DataType.SGS_PE:
+        script_name = pipeline_config.pe_script_path
+    elif args.data_type == DataType.SGS_SE:
+        script_name = pipeline_config.se_script_path
+    elif args.data_type == DataType.ARTIC_PE:
+        script_name = f"{pipeline_config.artic_script_path} --data_type PE"
+    else:
+        script_name = f"{pipeline_config.artic_script_path} --data_type SE"
 
-        process = subprocess.run(command_args)
+    # Get ref_db_path params
+    ref_db_path = args.ref_db_path
+    # Get snpeff_db_path
+    snpeff_db_path = args.snpeff_db_path
+    # Command to execute the Nextflow pipeline
+    command = (
+        f"nextflow run -name {pipeline_config.nextflow_project_name} -c {pipeline_config.nextflow_config_path} -work-dir {pipeline_config.nextflow_work_dir} {script_name} "
+        f"--outputs_dir {outputs_dir_path} --map_file {args.mapping_file_path} --ref_db_path {ref_db_path} --snpeff_db_path {snpeff_db_path}"
+    )
+    # For one-pool/Artic, we need to pass the primer filepath
+    if args.data_type in [DataType.ARTIC_PE, DataType.ARTIC_SE]:
+        command += f" --primer_file_path {args.primer_file_path}"
+    log(command)
+    # Run
+    command_args = shlex.split(command)
 
-        if process.returncode == 1:
-            # Nextflow somehow does not produce stderr, need the following to capture stderr
-            stderr_capture_command = (
-                f"nextflow log {pipeline_config.nextflow_project_name} -f stderr"
-            )
-            stderr_capture_result = subprocess.run(
-                shlex.split(stderr_capture_command),
-                stderr=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-            )
-            stderr_msg = stderr_capture_result.stdout.decode()
-            raise NephelePipelineError(stderr_msg)
-    except Exception:
-        exit_status = 1
-        error_msg = f"Pipeline Error:\n{traceback.format_exc()}"
-        # explicitly writing to stderr
-        print(error_msg, file=sys.stderr, flush=True)
-    finally:
-        try:
-            log("Removing intermediate dirs...")
-            dirs_to_keep = []
-            if args.get_bam_files:
-                dirs_to_keep.append("bam_files")
-            if args.data_type in [DataType.SGS_PE, DataType.SGS_SE]:
-                dirs_to_keep.extend(pipeline_config.sgs_dirs_to_keep)
-            else:
-                dirs_to_keep.extend(pipeline_config.artic_dirs_to_keep)
-            remove_intermediate_dirs(outputs_dir_path, dirs_to_keep)
-        except Exception as e:
-            log(f"Warning: clean up step error: {str(e)}")
+    process = subprocess.run(command_args)
 
-    # Report outputs
-    if report_outputs:
-        try:
-            pipeline.report_outputs(
-                Path(pipeline_config.outputs_template_file_name),
-                outputs_dir_path / pipeline_config.outputs_report_file_name,
-            )
-        except Exception:
-            log(f"Warning: error reporting outputs:\n {traceback.format_exc()}")
-
-    if exit_status == 0:
-        log("Pipeline completed")
-
-    exit(exit_status)
+    if process.returncode == 1:
+        # Nextflow somehow does not produce stderr, need the following to capture stderr
+        stderr_capture_command = (
+            f"nextflow log {pipeline_config.nextflow_project_name} -f stderr"
+        )
+        stderr_capture_result = subprocess.run(
+            shlex.split(stderr_capture_command),
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+        )
+        stderr_msg = stderr_capture_result.stdout.decode()
+        raise NephelePipelineError(stderr_msg)
 
 
 if __name__ == "__main__":
-    PARSER = argparse.ArgumentParser()
-    PARSER.add_argument(
-        "--version",
-        action="version",
-        version=pipeline_config.version,
-        help="show program's version number and exit",
-    )
-    PARSER.add_argument(
-        "--json_file_path",
-        type=Path,
-        required=True,
-        help=f"Input JSON file",
-    )
-    ARGS = PARSER.parse_args()
-    main(ARGS)
+    main()
---
+++ nephele_sars2/parser.py
@@ -0,0 +1,139 @@
+from argparse import ArgumentTypeError
+import csv
+from pathlib import Path
+
+from nephele_pipeline_utils.parser import NepheleArgumentParser
+from nephele_pipeline_utils.types import (
+    boolean,
+    directory,
+    path,
+    validate_path,
+    file_extensions,
+)
+
+from config import pipeline_config
+from enums import DataType
+
+
+class ArgumentParser(NepheleArgumentParser):
+    @staticmethod
+    def validate_data(args):
+        if args.data_type in (DataType.ARTIC_PE, DataType.ARTIC_SE):
+            if not args.primer_file_path:
+                raise ArgumentTypeError(
+                    "primer_file_path must be provided if data_type is ARTIC."
+                )
+
+        args.ref_db_path = Path(f"{args.ref_db_path}/SARS-CoV2.fa")
+
+    def parse_args(self, **kwargs):
+        args = super().parse_args(**kwargs)
+
+        # remove description from mapping file path if exists
+        fieldnames = list(args.samples[0].keys())
+        field_to_remove = "Description"
+        if field_to_remove in fieldnames:
+            fieldnames.remove(field_to_remove)
+            with open(args.mapping_file_path, "w", newline="") as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter="\t")
+                # Write the header
+                writer.writeheader()
+                # Write the data
+                for sample in args.samples:
+                    sample = sample.copy()
+                    del sample[field_to_remove]
+                    writer.writerow(sample)
+
+        return args
+
+    def get_samples(self, args):
+        samples, sample_ids = super().get_samples(args)
+        data_type = args.data_type
+
+        sample_files = []
+        if data_type == DataType.SGS_PE:
+            sample_files = [
+                "ForwardFastqFile_A",
+                "ForwardFastqFile_B",
+                "ReverseFastqFile_A",
+                "ReverseFastqFile_B",
+                "PrimerFile_A",
+                "PrimerFile_B",
+            ]
+        elif data_type == DataType.SGS_SE:
+            sample_files = [
+                "ForwardFastqFile_A",
+                "ForwardFastqFile_B",
+                "PrimerFile_A",
+                "PrimerFile_B",
+            ]
+        elif data_type == DataType.ARTIC_PE:
+            sample_files = ["ForwardFastqFile", "ReverseFastqFile"]
+        elif data_type == DataType.ARTIC_SE:
+            sample_files = ["ForwardFastqFile"]
+
+        for sample in samples:
+            for sample_file in sample_files:
+                if sample_file not in sample:
+                    raise ArgumentTypeError(
+                        f"{sample_file} must be present in the samples."
+                    )
+
+                file_type = "fa" if "Primer" in sample_file else "fastq"
+                validate_path(file_extensions(file_type), sample[sample_file])
+
+        return samples, sample_ids
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=pipeline_config.version,
+        help="show program's version number and exit",
+    )
+    parser.add_argument(
+        "--data_type",
+        type=str,
+        required=True,
+        choices=DataType.values(),
+        help="data type",
+    )
+    parser.add_argument(
+        "--ref_db_path",
+        type=directory,
+        required=True,
+        help="ref db path.",
+    )
+    parser.add_argument(
+        "--snpeff_db_path",
+        type=directory,
+        required=True,
+        help="snpeff db path.",
+    )
+    parser.add_argument(
+        "--get_bam_files",
+        type=boolean,
+        default=False,
+        help="get bam files",
+    )
+    parser.add_argument(
+        "--primer_file_path",
+        type=path("db"),
+        required=False,
+        help="primer db file path. Required for ARTIC data type",
+    )
+    parser.add_argument(
+        "--mapping_file_path",
+        type=path("csv"),
+        required=True,
+        help="Input path to mapping file",
+    )
+    parser.add_argument(
+        "--outputs_dir_path",
+        type=directory,
+        default=Path("/outputs"),
+        help="Path to outputs directory",
+    )
+    return parser.parse_args()
